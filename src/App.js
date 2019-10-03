3fefb0660cbf75db80f1f2fd15ec4873ed5d6edd
devohno <devohno.ko@gmail.com>
2019-10-04 01:31:44 +0900

    Init project & Add main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+
+const styles = {
+  container: {
+    width: 150,
+    height: 150,
+    // backgroundColor: "#282c34",
+    backgroundColor: "transparent",
+    border: "3px solid #09d3ac",
+    borderRadius: 2,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 5
+  },
+  logo: {
+    height: 90
+  }
+};
+
+function Container({ type }) {
+  const logos = {
+    rock: require("./assets/images/rock.png"),
+    paper: require("./assets/images/paper.png"),
+    scissors: require("./assets/images/scissors.png")
+  };
+  return (
+    <Button variant="contained" style={styles.container}>
+      <img src={logos[type]} style={styles.logo} />
+    </Button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container type="rock" />
+        <Container type="scissors" />
+        <Container type="paper" />
       </header>
     </div>
   );
